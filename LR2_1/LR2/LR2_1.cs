03488cec5105dd01f75594a02ab3b0c62853895f
KalininaEkaterina <79200863+KalininaEkaterina@users.noreply.github.com>
2021-04-03 09:27:36 +0300

    Add files via upload

---
+++ LR2_1/LR2/LR2_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace LR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str ;
+            Console.WriteLine("Введите строку ");
+            str = Console.ReadLine();
+            string[] words = str.Split(' ');
+            Console.WriteLine("В обратном порядке:");            
+            for (int i= words.Length-1; i >= 0; i--)
+            {
+                Console.Write(words[i] + " ");
+            }
+            Console.ReadKey();
+        }        
+    }
+}
