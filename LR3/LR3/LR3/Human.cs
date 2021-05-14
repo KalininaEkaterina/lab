b2614aa08d31ba26eb3c13201f6006edddba530d
KalininaEkaterina <79200863+KalininaEkaterina@users.noreply.github.com>
2021-05-14 20:53:06 +0300

    3laba

---
+++ LR3/LR3/LR3/Human.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR3
+{
+    class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Univercity { get; set; }
+        public string Specialty { get; set; }
+        public Human()
+        {
+            Name = Name;
+            Surname = Surname;
+        }
+    }
+}
