5519133e74f0e8a3f873da8a4e2f7793b22f5bdd
KalininaEkaterina <79200863+KalininaEkaterina@users.noreply.github.com>
2021-05-14 21:00:44 +0300

    3laba

---
+++ LR3/LR3/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace LR3
+{
+    class Person
+    {
+        Human[] data;
+        public Person()
+        {
+            data = new Human[10];
+        }
+        public Human this[int index1]
+        {
+            get
+            {
+                return data[index1];
+            }
+            set
+            {
+                data[index1] = value;
+            }
+        }
+    }
+}
---
+++ LR3/LR3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace LR3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Info inf = new Info();
+            Human hum = new Human();
+            int i = 0;
+            Person person = new Person();
+            int sw = 0;
+            while (sw != 3)
+            {
+                Console.WriteLine("1 - Получить информацию\n2 - Запись информации\n3- Выйти");
+                try
+                {
+                    sw = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.Clear();
+                }
+                switch (sw)
+                {
+                    case 1:
+                        Console.WriteLine("Введите имя");
+                        hum.Name = Console.ReadLine();
+                        Console.WriteLine("Введите фамилию");
+                        hum.Surname = Console.ReadLine();
+                        Console.WriteLine("Введите возраст");
+                        hum.Age = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите университет");
+                        hum.Univercity = Console.ReadLine();
+                        Console.WriteLine("Введите специальность");
+                        hum.Specialty = Console.ReadLine();
+                        inf.Student();
+                        i++;
+                        break;
+                    case 2:
+                        Console.WriteLine(hum.Name + " " + hum.Surname + " " + hum.Age + " лет " + hum.Univercity + " " + hum.Specialty);
+
+                        break;
+                    case 3:
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
